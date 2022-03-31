c480feb6c259476776119b43531c9074233ab4f5
Thorsten Fritze <1478200+tehaef@users.noreply.github.com>
2022-03-31 15:46:51 +0200

    Added MPL Licence info to source.

--- bin/add-modified.sh
+++ bin/add-modified.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# Copyright 2020-2022, UB JCS, Goethe University Frankfurt am Main
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 [[ $# != 2 ]] && { exit 1; }
 tmp="$2"
 cat "$1" > "$tmp"
